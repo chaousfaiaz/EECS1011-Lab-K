368f2e07eb090b3e8a14ae49042f40e75fe0cf86
chaousfaiaz <53284875+chaousfaiaz@users.noreply.github.com>
2025-04-17 17:39:45 -0400

    labKtest.m

--- LabK/LabK/labKtest.m
+++ LabK/LabK/labKtest.m
@@ -1,13 +1,13 @@
 %Replace YOUR_API_KEY_HERE with your API key from NASA!
-YOUR_APIKEY = "YOUR_API_KEY_HERE";
+YOUR_APIKEY = "O1qrqoRVf26xddrKcXa1EXWAJy7vwl4KP7XpQunj";
 
 count = 0;
 try 
-    data = getNEOs(2015,1,1,YOUR_APIKEY);
+    data = getNEOs(2015,1,1,O1qrqoRVf26xddrKcXa1EXWAJy7vwl4KP7XpQunj);
     assert(data.element_count == 14 | data.element_count == 15, 'Code reports wrong number of NEOs')
     count = count + 1;
 catch exception
-  fprintf('FAILED TEST 1: getObjects(2015,1,1,YOUR_APIKEY) returns too many or too few NEOs!\n')
+  fprintf('FAILED TEST 1: getObjects(2015,1,1,O1qrqoRVf26xddrKcXa1EXWAJy7vwl4KP7XpQunj) returns too many or too few NEOs!\n')
 end
 
 pause(1); %to avoid rapid subsequent API calls
@@ -23,7 +23,7 @@ end
 pause(1); %to avoid rapid subsequent API calls
 
 try 
-    data = getNEOs(2015,9,9,YOUR_APIKEY);
+    data = getNEOs(2015,9,9,O1qrqoRVf26xddrKcXa1EXWAJy7vwl4KP7XpQunj);
     value = hazardousNEOs(data);
     assert(value == true, 'one of the NEOs is hazardous!')
     count = count + 1;
